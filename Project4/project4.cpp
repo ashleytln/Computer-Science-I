b284d2e8bd22b9891ef07c2e794270354ae906c9
ashleytln <145338231+ashleytln@users.noreply.github.com>
2024-06-02 01:09:37 -0500

    Update project4.cpp

--- Project4/project4.cpp
+++ Project4/project4.cpp
@@ -22,7 +22,8 @@ double calculateExpression(double input, Node* head) {
 int main() {
 	ifstream infile;
 	string filename, line, input, coef, exp;
-
+    double val;
+    
 	Node* term = nullptr;
 	unsigned i = 0;
 
@@ -31,7 +32,6 @@ int main() {
 
 	infile.open(filename);
 	if (infile.good()) {
-
 		while (getline(infile, line)) {
 			LinkedList expression;
 			input = "", coef = "", exp = "";
@@ -41,54 +41,64 @@ int main() {
 			// Traverse to expression
 			i += 4;
 			while (i < line.length()) {
-				// If term is not last term
-				//if ((line.find(' ', i) != std::string::npos) && (line.find(' ', i) <= (i + 9))) {
-				//}
 				// Term contains coefficient other than 1
 				if ((line.find('x', i) != std::string::npos) && (line.find('x', i) <= (i + 4))) {
 					for (; i < line.find('x', i); i++) {
 						coef += line[i];
 					}
 				}
-				// Term contains exponent other than 0
+				// Term contains exponent other than 0 or 1
 				if ((line.find('^', i) != std::string::npos) && (line.find('^', i) <= (i + 1))) {
-					i = line.find('^', i) + 1;
+					i = static_cast<int>(line.find('^', i)) + 1;
+					// Term is not at end of expression
 					if (line.find(' ', i) != std::string::npos) {
 						for (; i < line.find(' ', i); i++) {
 							exp += line[i];
 						}
 					}
+					// Term is at end of expression (anything after '^' is exponent)
 					else {
 						for (; i < line.length(); i++) {
 							exp += line[i];
 						}
 					}
 				}
-				// Term contains zero exponent
-				if (line.find('^', i) == std::string::npos) {
+				// Term is a constant (non-zero coefficient, zero exponent)
+				else if (line.find('^', i) == std::string::npos && line.find('x', i) == std::string::npos) {
 					for (; i < line.length(); i++) {
 						coef += line[i];
 					}
-				}
-				if (coef == "")
-					coef = "1";
-				if (exp == "")
 					exp = "0";
+				}
+				// Term coefficient is empty
+				if (coef == "")
+				    coef += "1";
+				// Term exponent is empty
+				if (exp == "")
+				    exp += "1";
+				// Create term node to add to expression linked list
 				term = new Node(stod(coef), stoi(exp));
 				term->setNext(nullptr);
+				// Add node term to expression linked list
 				expression += term;
+				// Reset coefficient and exponent variables
 				coef = "", exp = "";
-				if ((i + 1) <= (line.length() - 1)) {
+				// If current index not past end of line
+				if (i <= (line.length() - 1)) {
 					i++;
+					// If index past end of line, stop reading
+					if (i >= line.length())
+					    break;
+					// If current element is '-', make coefficient negative 
 					if (line[i] == '-')
-						coef += line[i];
+						coef += "-";
+					// Traverse to next term in expression
 					i += 2;
 				}
-				if (i >= line.length())
-					break;
 			}
+			// Sort expression linked list based on exponent (standard form)
 			expression.sortList();
-			double val = calculateExpression(stod(input), expression.getHead());
+			val = calculateExpression(stod(input), expression.getHead());
 			cout << setprecision(3) << fixed;
 			cout << expression << " = " << val << endl;
 		}
